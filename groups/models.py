b8b0345d529875217be7997736631ad6e170dbdf
Felps <felipmartins94@gmail.com>
2023-05-04 11:31:14 -0300

    adiciona modelo de grupo

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Group(models.Model):
+
+    title = models.CharField(max_length=150)
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f"{self.title} - {self.users.all()}"
