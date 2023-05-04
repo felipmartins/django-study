7aa3cdd5a07c670ab00bdf5c8d68f160ba22d4d9
Felps <felipmartins94@gmail.com>
2023-05-04 11:25:13 -0300

    adiciona funcionalidade para inserir no banco

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from accounts.form import CreateUserForm
+from accounts.models import User
 
 def index(request):
+
+    if request.method == "POST":
+        user_form = CreateUserForm(request.POST)
+
+        if user_form.is_valid():
+            user_data = user_form.cleaned_data
+            user = User.objects._create_employe(**user_data)
+
     context = {"user_form": CreateUserForm()}
     return render(request, "index.html", context)
