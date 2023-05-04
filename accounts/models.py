b3d2a0d3d549eb1abe77e25d2e14ca85d366d318
Felps <felipmartins94@gmail.com>
2023-05-04 10:46:08 -0300

    Adiciona modelos criados para pessoas usuarias

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+
+    def _create_employe(self, username, email, password, **extra_fields):
+        user = super._create_user(self, username, email, password, **extra_fields)
+        user.is_staff = True
+        user.save()
+        return user
+
+    def _create_manager(self, username, email, password, **extra_fields):
+        user = super._create_user(self, username, email, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractUser):
+    
+    objects = CustomUserManager()
+
