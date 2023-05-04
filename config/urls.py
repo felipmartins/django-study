035065af66ee6f338a1c624e50541450d3ca7f95
Felps <felipmartins94@gmail.com>
2023-05-04 12:03:22 -0300

    adiciona tudo que é necessario para usar o widget

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django import views as django_views
 
 urlpatterns = [
     path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('group/', include('groups.urls')),
+    re_path(r'^jsi18n/$', django_views.i18n.JavaScriptCatalog.as_view(), name='jsi18n'),
 ]
--- groups/forms.py
+++ groups/forms.py
@@ -1,10 +1,25 @@
 from django import forms
 from django.db import models
+from django.contrib.admin.widgets import FilteredSelectMultiple
 from groups.models import Group
+from accounts.models import User
 
 
 class CreateGroupForm(forms.ModelForm):
-    
+
+    class Media:
+        css = {
+            "all": ('/static/admin/css/widgets.css',)
+        }
+        js = ("/admin/jsi18n",)
+
+    users = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        widget=FilteredSelectMultiple("Pessoas usuárias", False),
+        required = True
+    )
+
+
     class Meta:
         model = Group
         fields = "__all__"
