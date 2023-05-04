7ac29aae19f7aa3aaa40d737bffd645818d0f7a7
Felps <felipmartins94@gmail.com>
2023-05-04 11:25:00 -0300

    refatora administrador do modelo

--- accounts/models.py
+++ accounts/models.py
@@ -4,14 +4,20 @@ from django.contrib.auth.models import AbstractUser, UserManager
 
 class CustomUserManager(UserManager):
 
-    def _create_employe(self, username, email, password, **extra_fields):
-        user = super._create_user(self, username, email, password, **extra_fields)
+    def _create_employe(self, **fields):
+        username = fields.pop("username")
+        email = fields.pop("email")
+        password = fields.pop("password")
+        user = super()._create_user(username, email, password, **fields)
         user.is_staff = True
         user.save()
         return user
 
-    def _create_manager(self, username, email, password, **extra_fields):
-        user = super._create_user(self, username, email, password, **extra_fields)
+    def _create_manager(self, **fields):
+        username = fields.pop("username")
+        email = fields.pop("email")
+        password = fields.pop("password")
+        user = super()._create_user(username, email, password, **fields)
         user.is_staff = True
         user.is_superuser = True
         user.save()
@@ -19,6 +25,6 @@ class CustomUserManager(UserManager):
 
 
 class User(AbstractUser):
-    
+
     objects = CustomUserManager()
 
