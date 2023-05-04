27c226266b6dd65e8b00edfe81c32e456196c8eb
Felps <felipmartins94@gmail.com>
2023-05-04 11:47:24 -0300

    adiciona conteudo no template e na view para ficar similar a de user

--- groups/views.py
+++ groups/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from groups.forms import CreateGroupForm
+from groups.models import Group
 
 
 def group_index(request):
-    context = {"group_form": CreateGroupForm()}
+
+    if request.method == "POST":
+        group_form = CreateGroupForm(request.POST)
+
+        if group_form.is_valid():
+            
+            group_form.save() 
+            
+    context = {"group_form": CreateGroupForm(), "groups": Group.objects.all()}
     return render(request, "groups/index.html", context)
