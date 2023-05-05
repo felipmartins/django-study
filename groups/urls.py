c4504cf9732189544a5b0cb514bc11441cae2fae
Felps <felipmartins94@gmail.com>
2023-05-05 20:34:47 -0300

    vincula a view de resgate de grupo a url

--- groups/urls.py
+++ groups/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
-from groups.views import group_index, create_group
+from django.urls import path
+from groups.views import group_index, create_group, retrieve_group
 
 
 urlpatterns = [
     path('', group_index, name="group-index"),
-    path('group/new', create_group, name="new-group")
+    path('group/new', create_group, name="new-group"),
+    path('group/<int:group_id>', retrieve_group, name="group-details"),
 ]
