b2c6ed111a86957180b84fb118cc2a88a5b4e1bd
Felps <felipmartins94@gmail.com>
2023-05-05 17:37:13 -0300

    vincula nova view à user/new

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from accounts.views import index
+from accounts.views import index, create_user
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="index"),
+    path('user/new', create_user, name="new-user")
 ]
