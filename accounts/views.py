d8c8613789cbecaa4eb27ff7d832248466f37e31
Felps <felipmartins94@gmail.com>
2023-05-05 17:32:33 -0300

    separa funcionalidade de criação users em outra view

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from accounts.form import CreateUserForm
 from accounts.models import User
 
 def index(request):
 
-    if request.method == "POST":
+    context = {"users": User.objects.all()}
+    return render(request, "accounts/index.html", context)
+
+
+def create_user(request):
+
+    if request.method== "POST":
         user_form = CreateUserForm(request.POST)
 
         if user_form.is_valid():
             user_data = user_form.cleaned_data
-            user = User.objects._create_employe(**user_data)
+            User.objects._create_employe(**user_data)
 
-    context = {"user_form": CreateUserForm(), "users": User.objects.all()}
-    return render(request, "accounts/index.html", context)
+        redirect("index")
+    
+    context = {"user_form": CreateUserForm()}
+
+    return render(request, "accounts/create_user.html", context)
