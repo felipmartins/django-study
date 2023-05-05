b4c30c13fd3317475aaa6c5a480cbf05a0d1a8ff
Felps <felipmartins94@gmail.com>
2023-05-05 20:35:08 -0300

    corrige duplicado nas urls dos grupos

--- groups/urls.py
+++ groups/urls.py
@@ -4,6 +4,6 @@ from groups.views import group_index, create_group, retrieve_group
 
 urlpatterns = [
     path('', group_index, name="group-index"),
-    path('group/new', create_group, name="new-group"),
-    path('group/<int:group_id>', retrieve_group, name="group-details"),
+    path('/new', create_group, name="new-group"),
+    path('/<int:group_id>', retrieve_group, name="group-details"),
 ]
