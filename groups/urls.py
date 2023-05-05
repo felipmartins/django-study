b287f8ba8d064178bdc5d53c32260f34002ccc68
Felps <felipmartins94@gmail.com>
2023-05-05 19:31:58 -0300

    vincula nova view a group/new

--- groups/urls.py
+++ groups/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from groups.views import group_index
+from groups.views import group_index, create_group
 
 
 urlpatterns = [
-    path('', group_index)
+    path('', group_index, name="group-index"),
+    path('group/new', create_group, name="new-group")
 ]
