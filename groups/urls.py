a140470685ed7f8ee0348149c16e54355fa45fc5
Felps <felipmartins94@gmail.com>
2023-05-05 20:58:21 -0300

    adiciona url para atualização do grupo

--- groups/urls.py
+++ groups/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from groups.views import group_index, create_group, retrieve_group
+from groups.views import group_index, create_group, retrieve_group, update_group_users
 
 
 urlpatterns = [
     path('', group_index, name="group-index"),
     path('new', create_group, name="new-group"),
     path('<int:group_id>', retrieve_group, name="group-details"),
+    path('update-users/<int:group_id>', update_group_users, name="update-group"),
 ]
