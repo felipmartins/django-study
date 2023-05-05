d104c135184045ebcc1201a83b638421785f51f8
Felps <felipmartins94@gmail.com>
2023-05-05 19:31:43 -0300

    separa função em duas

--- groups/views.py
+++ groups/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from groups.forms import CreateGroupForm
 from groups.models import Group
 
 
 def group_index(request):
 
+    context = {"groups": Group.objects.all()}
+    return render(request, "groups/index.html", context)
+
+
+def create_group(request):
+
     if request.method == "POST":
         group_form = CreateGroupForm(request.POST)
 
         if group_form.is_valid():
-            
             group_form.save() 
-            
-    context = {"group_form": CreateGroupForm(), "groups": Group.objects.all()}
-    return render(request, "groups/index.html", context)
+            return redirect('group-index')
+
+    context = {"group_form": CreateGroupForm()}
+
+    return render(request, "groups/create_group.html", context)
