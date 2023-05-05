13586b9ae1e0d6a9a4053f4e5a5690f62e47be42
Felps <felipmartins94@gmail.com>
2023-05-05 20:57:54 -0300

    implementa view para update de grupo

--- groups/views.py
+++ groups/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from groups.forms import CreateGroupForm
+from groups.forms import CreateGroupForm, UpdateUsersGroupForm
 from groups.models import Group
+from accounts.models import User
 
 
 def group_index(request):
@@ -26,3 +27,27 @@ def retrieve_group(request, group_id):
     
         context = {"group": Group.objects.get(id=group_id)}
         return render(request, "groups/group_details.html", context)
+
+def update_group_users(request, group_id):    
+
+    if request.method == "POST":
+        if "users" in request.POST:
+            users = {User.objects.get(id=user) for user in dict(request.POST)['users']}
+        else:
+            users = set()
+        group = Group.objects.get(id=group_id)
+        users_before = set(group.users.all())
+        print(users_before)
+        for user in users:
+            group.users.add(user)
+        users_to_remove = users_before - users
+
+        for user in users_to_remove:
+            group.users.remove(user)
+             
+        group.save()
+        return redirect('group-details', group_id=group_id)
+    
+    context = {"group_form": UpdateUsersGroupForm(group_id=group_id)}
+
+    return render(request, "groups/update_group_users.html", context)
