956eec4afb4811c94d0620c13e362c91397fc3f8
Felps <felipmartins94@gmail.com>
2023-05-05 21:05:09 -0300

    vincula view a url de deleção

--- groups/urls.py
+++ groups/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from groups.views import group_index, create_group, retrieve_group, update_group_users
+from groups.views import group_index, create_group, retrieve_group, update_group_users, delete_group
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('new', create_group, name="new-group"),
     path('<int:group_id>', retrieve_group, name="group-details"),
     path('update-users/<int:group_id>', update_group_users, name="update-group"),
+    path('delete/<int:group_id>', delete_group, name="delete-group"),
 ]
