a7167a7a0944c5fd390dbae7b26a57aa5a764de9
Felps <felipmartins94@gmail.com>
2023-05-09 15:22:00 -0300

    adiciona configuração de login

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.User"
+
+LOGIN_URL = "login"
+LOGOUT_REDIRECT_URL = LOGIN_URL
+LOGIN_REDIRECT_URL = "index"
