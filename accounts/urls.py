c1950d5d025f31bc1aa52b97ef8fa212ea00117c
Felps <felipmartins94@gmail.com>
2023-05-09 15:23:18 -0300

    adiciona as urls para telas de login e logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from accounts.views import index, create_user
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', index, name="index"),
-    path('user/new', create_user, name="new-user")
+    path('user/new', create_user, name="new-user"),
+    path('login/', LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path('logout/', LogoutView.as_view(template_name="accounts/logout.html"), name="logout"),
+
 ]
